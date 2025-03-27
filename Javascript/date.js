48a74507eb1fddc3281844dc251275314405bace
ADISHESHA-R <129973855+ADISHESHA-R@users.noreply.github.com>
2025-03-27 05:59:30 +0000

    date functions

---
+++ Javascript/date.js
@@ -0,0 +1,9 @@
+//DATES 
+let myDate=new Date();
+console.log(myDate.toString());
+let mcd=new Date(2025,3,21);
+console.log(mcd.toDateString());
+// console.log(mcd.getMonth());
+// console.log(mcd.getDay());
+let timestamp=new Date();
+console.log(timestamp.toLocaleString());
