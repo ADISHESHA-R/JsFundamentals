329421496026a067b96b6d6d60e21686f6318de2
ADISHESHA-R <129973855+ADISHESHA-R@users.noreply.github.com>
2025-03-31 06:25:53 +0000

    filter

---
+++ Javascript/filter.js
@@ -0,0 +1,16 @@
+let arr=[1,2,3,4,5,6,7,8,9];
+const ee=arr.filter((n)=>n>4);
+console.log(ee);
+
+//map
+const nets=arr.map((r)=>r*10).map((r)=>r+1).filter((r)=>r>21&&r<91);
+console.log(nets);
+
+//reduce
+const mytotal=arr.reduce((acc,cum)=>acc+cum,0);
+console.log(mytotal);
+
+const sa=arr.map((a)=>a+3).filter((a)=>a>2);
+console.log(sa);
+
+   
