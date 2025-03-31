32312732155447db0cdab27dc8e3f10a0376421c
ADISHESHA-R <129973855+ADISHESHA-R@users.noreply.github.com>
2025-03-31 16:56:37 +0000

    functions

---
+++ Javascript/Functions/1.js
@@ -0,0 +1,30 @@
+function sayName(){
+    console.log("A");
+    console.log("d");
+    console.log("i");
+console.log("s");
+console.log("h");
+console.log("e");
+console.log("s");
+console.log("h");
+  console.log("a"); 
+  console.log(".R");
+   
+}
+// sayName();
+
+// function login(username){
+//     if(!username){
+//         console.log("Please enter username");
+//          return ;
+//     }
+//     else{
+//         return `${username} is succesfully logeed in`;
+//     }
+// }
+// console.log(login("adishesha"));
+
+function calculateCartPrice(...val){
+return val;
+}
+console.log(calculateCartPrice(200,300,400));
