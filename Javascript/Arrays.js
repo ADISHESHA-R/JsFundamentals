72a209a4ae5baa81d1b62bea15262b1515ccefba
ADISHESHA-R <129973855+ADISHESHA-R@users.noreply.github.com>
2025-04-08 13:18:26 +0530

    Update Arrays.js

--- Javascript/Arrays.js
+++ Javascript/Arrays.js
@@ -19,7 +19,7 @@ let arr1=[1,2,3];
 let arr2=[4,5,6];
 // let arr3=arr1+arr2;
 // let arr3=arr1.concat(arr2);
-let arr3=[...arr1,...arr2];
+let arr3=[...arr1,...arr2];//...arr1,...arr2 means spread Operator
 console.log(arr3);
 
 //multiple array elements into an single array
